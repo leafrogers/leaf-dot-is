c10f287711124319bbd0423a66264a4769bbe127
Leaf Rogers <150157+leafrogers@users.noreply.github.com>
2022-06-29 12:50:32 +0100

    feat: make sure config vars are defined before allowing the app to start

--- server/config.js
+++ server/config.js
@@ -1,7 +1,12 @@
+// @ts-nocheck
+import { assertNoUndefinedValues } from './helpers.js';
+
 const config = {
 	APP_FRIENDLY_NAME: 'fill-out--your-user-facing-app-name',
 	IS_PRODUCTION: process.env.NODE_ENV === 'production',
 	PORT: process.env.PORT || 3001
 };
+
+assertNoUndefinedValues(config);
 
 export default config;
--- server/helpers.js
+++ server/helpers.js
@@ -1,3 +1,24 @@
+/**
+ * @param {object} object
+ */
+export const assertNoUndefinedValues = (object) => {
+	const keysWithUndefinedValues = [];
+
+	for (const [key, value] of Object.entries(object)) {
+		if (typeof value === 'undefined') {
+			keysWithUndefinedValues.push(key);
+		}
+	}
+
+	if (keysWithUndefinedValues.length) {
+		throw new Error(
+			`Expected all config variables to be defined but found the following undefined variables: ${keysWithUndefinedValues.join(
+				', '
+			)}.`
+		);
+	}
+};
+
 export const logger = console;
 
 /**
