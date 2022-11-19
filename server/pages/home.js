4cc9a1c9dd919dca4f14cccc75e3f194de9a9b9c
Leaf Rogers <150157+leafrogers@users.noreply.github.com>
2022-11-19 21:26:17 +0000

    feat: update homepage text
    Adds some text and links to my different social media profiles.

--- server/pages/home.js
+++ server/pages/home.js
@@ -6,7 +6,7 @@ import { toHtmlDocString } from '../helpers.js';
  */
 export const controller = async (_req, res) => {
 	const data = {
-		text: { title: '🍃' }
+		text: { title: '🍂 Hello I am Leaf' }
 	};
 
 	res.send(view(data));
@@ -18,6 +18,15 @@ export const controller = async (_req, res) => {
 const view = ({ text }) => {
 	const body = `
 		<h1>${text.title}</h1>
+		<p>One day I will make this nice. Find me on:</p>
+		<ul>
+			<li><a rel="me" href="https://neurodifferent.me/@leaf">Mastodon</a></li>
+			<li><a href="https://twitter.com/LeafRogers">Twitter</a> until stocks last</li>
+			<li><a href="https://github.com/leafrogers">GitHub</a> although my current work is in a private GitLab account, boo</li>
+			<li><a href="https://www.linkedin.com/in/leafrogers">LinkedIn</a> if that’s your thing</li>
+		</ul>
+		<p>I’m unavailable for work contracts until May 2023.</p>
+		<p>This content is a bit dry isn’t it! Yeah bit dry. Anyway you look nice today.</p>
 	`.trim();
 
 	return toHtmlDocString({
