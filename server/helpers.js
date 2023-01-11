cce10d39b510d2e971bf894eb595ffd28cb6ada9
Leaf Rogers <150157+leafrogers@users.noreply.github.com>
2023-01-11 23:29:32 +0000

    feat: apply new styling pizzazz!

--- server/helpers.js
+++ server/helpers.js
@@ -70,6 +70,8 @@ export const toHtmlDocString = ({ body, styles = '', title }) => {
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="${config.APP_FRIENDLY_DESCRIPTION}">
+		<meta name="theme-color" content="#fff0e3" media="(prefers-color-scheme: light)">
+		<meta name="theme-color" content="#08111c" media="(prefers-color-scheme: dark)">
 		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
